83bfd28ae78223bf95d1a808ba208322dc7953b4
Zvonimir Lokmer <lokmer@gmail.com>
2024-03-11 20:46:49 +0100

    Initial commit

---
+++ Ispit.Konzola/PametniTelefon.cs
@@ -0,0 +1,49 @@
+﻿using Ispit.Konzola.Sucelja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    public class PametniTelefon : IPametniTelefon
+    {
+        public string Model { get; private set; }
+
+        public PametniTelefon(string model)
+        {
+            Model = model;
+        }
+
+        public string Surfaj(string url)
+        {
+            if (ProvjeriAkoJeValidanUrl(url))
+            {
+                return $"URL {url} je ispravan.";
+            }
+
+            return "Neispravan URL!";
+        }
+
+        public string Poziv(string telefonski_broj)
+        {
+            if (ProvjeriAkoJeValidanBroj(telefonski_broj))
+            {
+                return $"Zovem: {telefonski_broj}";
+            }
+
+            return "Neispravan telefonski broj!";
+        }
+
+        private bool ProvjeriAkoJeValidanUrl(string url)
+        {
+            return !url.Any(char.IsDigit);
+        }
+
+        private bool ProvjeriAkoJeValidanBroj(string telefonski_broj)
+        {
+            return telefonski_broj.All(char.IsDigit);
+        }
+    }
+}
---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ispit.Konzola.Sucelja;
+using Ispit.Konzola;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PametniTelefon telefon = new PametniTelefon("iPhone 15 Plus");
+        TestirajTelefon(telefon);
+    }
+
+    static void TestirajTelefon(IPametniTelefon telefon)
+    {
+        Console.WriteLine("Unesite telefonski broj:");
+        string broj = Console.ReadLine();
+
+        Console.WriteLine("Unesite URL:");
+        string url = Console.ReadLine();
+
+        Console.WriteLine(telefon.Poziv(broj));
+        Console.WriteLine(telefon.Surfaj(url));
+    }
+}
